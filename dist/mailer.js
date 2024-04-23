9900f364afdc7f912d952afc6fe1afc1dda7ca60
Griffin Ferguson <griffinferguson.us@gmail.com>
2024-04-23 14:44:23 -0500

    update for 2024, add html emails

--- dist/mailer.js
+++ dist/mailer.js
@@ -7,24 +7,17 @@ exports.sendMessages = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
 function sendMessages(messages) {
     require("dotenv").config();
-    const transporter = nodemailer_1.default.createTransport({
-        host: "mail.memorialacademy.org",
-        port: 465,
-        secure: false,
-        auth: {
-            user: process.env.USERNAME,
-            pass: process.env.PASSWORD,
-        },
-        pool: true
-    });
-    transporter.sendMail({
-        from: "letscode@memorialacademy.org",
+    const transporter = nodemailer_1.default.createTransport(`smtps://${process.env.USERNAME}:${process.env.PASSWORD}@mail.memorialacademy.org:25`);
+    const sender = transporter.sendMail({
+        from: "admin@memorialacademy.org",
         to: "griffin.ferguson@memorialacademy.org",
         subject: messages[0].subject,
         text: messages[0].text,
         html: messages[0].html
     });
-    transporter.close();
-    console.log("sent");
+    sender.finally(() => {
+        console.log("sent");
+        transporter.close();
+    });
 }
 exports.sendMessages = sendMessages;
--- src/mailer.ts
+++ src/mailer.ts
@@ -10,24 +10,27 @@ export interface Message {
 
 export function sendMessages(messages: Array<Message>): void {
     require("dotenv").config();
-    const transporter = nodemailer.createTransport({
+    /* const transporter = nodemailer.createTransport({
         host: "mail.memorialacademy.org",
-        port: 465,
+        port: 587,
         secure: false,
         auth: {
           user: process.env.USERNAME,
           pass: process.env.PASSWORD,
         },
-        pool: true
-    });
+        // pool: true
+    });*/
+    const transporter = nodemailer.createTransport(`smtps://${process.env.USERNAME}:${process.env.PASSWORD}@mail.memorialacademy.org:25`);
 
-    transporter.sendMail({
-        from: "letscode@memorialacademy.org",
+    const sender = transporter.sendMail({
+        from: "admin@memorialacademy.org",
         to: "griffin.ferguson@memorialacademy.org",
         subject: messages[0].subject,
         text: messages[0].text,
         html: messages[0].html
     })
-    transporter.close();
-    console.log("sent")
+    sender.finally(() => {
+        console.log("sent");
+        transporter.close();
+    })
 }
